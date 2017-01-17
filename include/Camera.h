1b0d650617c0e0c13e6362cd4c10974bebb27229
Alok Hota <ahota@vols.utk.edu>
2017-01-16 21:28:11 -0500

    basic camera with view direction maintenance haven't tested anything yet!

---
+++ include/Camera.h
@@ -0,0 +1,38 @@
+#ifndef PBNJ_CAMERA_H
+#define PBNJ_CAMERA_H
+
+#include <pbnj.h>
+
+#include <ospray/ospray.h>
+
+namespace pbnj {
+
+    class Camera {
+        public:
+            Camera(int width, int height);
+
+            void setPosition(float x, float y, float z);
+            void setOrbitRadius(float radius);
+
+            //some sort of setPath function that takes an enum type for the path
+            //and a ... for path parameters
+
+            int imageWidth;
+            int imageHeight;
+
+        private:
+            float xPos;
+            float yPos;
+            float zPos;
+            float viewX;
+            float viewY;
+            float viewZ;
+            float orbitRadius;
+
+            OSPCamera oCamera;
+
+            void updateOSPRayPosition();
+    };
+}
+
+#endif
---
+++ src/Camera.cpp
@@ -0,0 +1,51 @@
+#include "Camera.h"
+
+#include <cmath>
+
+#include <ospray/ospray.h>
+
+namespace pbnj {
+
+Camera::Camera(int width, int height) :
+    imageWidth(width), imageHeight(height), xPos(0.0), yPos(0.0), zPos(0.0),
+    viewX(0.0), viewY(0.0), viewZ(0.0), orbitRadius(0.0)
+{
+    this->oCamera = ospNewCamera("perspective");
+    this->updateOSPRayPosition();
+    ospSet3fv(this->oCamera, "up",  (float[]){0.0, 1.0, 0.0});
+    ospSetf(this->oCamera, "aspect", (float)this->imageWidth/imageHeight);
+    ospCommit(this->oCamera);
+}
+
+void Camera::setOrbitRadius(float radius)
+{
+    this->orbitRadius = radius;
+    this->updateOSPRayPosition();
+}
+
+void Camera::setPosition(float x, float y, float z)
+{
+    this->xPos = x;
+    this->yPos = y;
+    this->zPos = z;
+    this->updateOSPRayPosition();
+}
+
+void Camera::updateOSPRayPosition()
+{
+    float deltaX = (this->xPos-this->viewX);
+    float deltaY = (this->yPos-this->viewY);
+    float deltaZ = (this->zPos-this->viewZ);
+    float mag = std::sqrt(deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ);
+    this->viewX = -deltaX/mag;
+    this->viewY = -deltaY/mag;
+    this->viewZ = -deltaZ/mag;
+
+    ospSet3fv(this->oCamera, "pos",
+            (float[]){this->xPos, this->yPos, this->zPos});
+    ospSet3fv(this->oCamera, "dir",
+            (float[]){this->viewX, this->viewY, this->viewZ});
+    ospCommit(this->oCamera);
+}
+
+}
