6db5d8f15cca62ba41b40a3a194b22e8d03009e5
Alok Hota <ahota@vols.utk.edu>
2017-03-17 15:06:39 -0500

    LRU cache for time series volumes

--- include/TimeSeries.h
+++ include/TimeSeries.h
@@ -3,7 +3,9 @@
 
 #include "Volume.h"
 
+#include <list>
 #include <string>
+#include <sys/sysinfo.h>
 #include <vector>
 
 namespace pbnj {
@@ -23,10 +25,20 @@ namespace pbnj {
             int xDim;
             int yDim;
             int zDim;
+            unsigned int dataSize;
+            unsigned int maxVolumes;
+            unsigned int currentVolumes;
+            std::list<int> lruCache;
+            void encache(unsigned int index);
+            void setMaxMemory(unsigned int gigabytes);
+
             unsigned int length;
             std::vector<std::string> dataFilenames;
             std::string dataVariable;
             Volume **volumes;
+
+            struct sysinfo systemInfo;
+            void initSystemInfo();
     };
 
 }
--- src/TimeSeries.cpp
+++ src/TimeSeries.cpp
@@ -2,27 +2,30 @@
 #include "Volume.h"
 
 #include <iostream>
+#include <sys/sysinfo.h>
 
 namespace pbnj {
 
 TimeSeries::TimeSeries(std::vector<std::string> filenames,
         int x, int y, int z) :
     dataFilenames(filenames), length(filenames.size()), xDim(x), yDim(y),
-    zDim(z)
+    zDim(z), dataSize(x*y*z*4)
 {
     this->volumes = new Volume*[this->length];
     for(int i = 0; i < this->length; i++)
         this->volumes[i] = NULL;
+    this->initSystemInfo();
 }
 
 TimeSeries::TimeSeries(std::vector<std::string> filenames,
         std::string varname, int x, int y, int z) :
     dataFilenames(filenames), length(filenames.size()), dataVariable(varname),
-    xDim(x), yDim(y), zDim(z)
+    xDim(x), yDim(y), zDim(z), dataSize(x*y*z*4)
 {
     this->volumes = new Volume*[this->length];
     for(int i = 0; i < this->length; i++)
         this->volumes[i] = NULL;
+    this->initSystemInfo();
 }
 
 TimeSeries::~TimeSeries()
@@ -32,6 +35,63 @@ TimeSeries::~TimeSeries()
             delete this->volumes[i];
             this->volumes[i] = NULL;
         }
+    }
+}
+
+void TimeSeries::initSystemInfo()
+{
+    //system data
+    //struct sysinfo system_info;
+    sysinfo(&(this->systemInfo));
+    unsigned long totalBytes, freeBytes, maxUsage;
+    totalBytes = this->systemInfo.mem_unit * this->systemInfo.totalram / 1073741824; //GB
+    freeBytes = this->systemInfo.mem_unit * this->systemInfo.freeram / 1073741824; // GB
+    maxUsage = this->systemInfo.mem_unit * this->systemInfo.freeram * 0.5; // bytes
+    std::cout << "DEBUG: Limiting maxVolumes" << std::endl;
+    this->maxVolumes = maxUsage / this->dataSize;
+    std::cout << "DEBUG: Total memory: " << totalBytes << "GB" << std::endl;
+    std::cout << "DEBUG: Free memory:  " << freeBytes << "GB" << std::endl;
+    std::cout << "DEBUG: Max to use:   " << maxUsage/1073741824 << "GB" << std::endl;
+    std::cout << "DEBUG: Concurrent vols: " << maxVolumes << std::endl;
+}
+
+void TimeSeries::setMaxMemory(unsigned int gigabytes)
+{
+    unsigned long freeBytes = this->systemInfo.mem_unit * this->systemInfo.freeram / 1073741824; // GB
+    if(gigabytes > freeBytes) {
+        std::cerr << "WARNING: Asking to use more memory than is currently ";
+        std::cerr << "available. Keeping limit at previous value" << std::endl;
+        return;
+    }
+    this->maxVolumes = gigabytes / this->dataSize;
+}
+
+void TimeSeries::encache(unsigned int index)
+{
+    if(this->lruCache.size() >= this->maxVolumes) {
+        std::cout << "DEBUG: Cache is full " << this->lruCache.size();
+        std::cout << " " << this->maxVolumes << std::endl;
+        // check if the LRU is this index
+        if(this->lruCache.front() == index) {
+            std::cout << "DEBUG: this was the LRU " << index << std::endl;
+            this->lruCache.remove(index);
+            // and put it at the end
+            this->lruCache.push_back(index);
+        }
+        else {
+            std::cout << "DEBUG: deleting LRU " << this->lruCache.front() << std::endl;
+            // delete the LRU volume
+            delete this->volumes[this->lruCache.front()];
+            this->volumes[this->lruCache.front()] = NULL;
+            this->lruCache.pop_front();
+        }
+    }
+    else {
+        std::cout << "DEBUG: pushed to back " << index << std::endl;
+        // remove this index if it was already here
+        this->lruCache.remove(index);
+        // and put it at the end
+        this->lruCache.push_back(index);
     }
 }
 
@@ -50,6 +110,7 @@ Volume *TimeSeries::getVolume(unsigned int index)
         else
             this->volumes[index] = new Volume(this->dataFilenames[index],
                     this->dataVariable, this->xDim, this->yDim, this->zDim);
+        this->encache(index);
     }
     
     return this->volumes[index];
