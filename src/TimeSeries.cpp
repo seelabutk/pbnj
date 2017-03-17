40ce44471237b92f9a9a7d342d2c62cd0402a2a1
Alok Hota <ahota@vols.utk.edu>
2017-03-17 17:56:02 -0500

    cleanup, removing debug messages

--- src/TimeSeries.cpp
+++ src/TimeSeries.cpp
@@ -30,10 +30,8 @@ TimeSeries::TimeSeries(std::vector<std::string> filenames,
 
 TimeSeries::~TimeSeries()
 {
-    std::cout << "DEBUG: TimeSeries destructor" << std::endl;
     for(int i = 0; i < this->length; i++) {
         if(this->volumes[i] != NULL) {
-            std::cout << "DEBUG: TimeSeries destructor deleting " << i << std::endl;
             delete this->volumes[i];
             this->volumes[i] = NULL;
         }
@@ -46,15 +44,13 @@ void TimeSeries::initSystemInfo()
     //struct sysinfo system_info;
     sysinfo(&(this->systemInfo));
     unsigned long totalBytes, freeBytes, maxUsage;
-    totalBytes = this->systemInfo.mem_unit * this->systemInfo.totalram / 1073741824; //GB
-    freeBytes = this->systemInfo.mem_unit * this->systemInfo.freeram / 1073741824; // GB
-    maxUsage = this->systemInfo.mem_unit * this->systemInfo.freeram * 0.5; // bytes
-    std::cout << "DEBUG: Limiting maxVolumes" << std::endl;
+    totalBytes = this->systemInfo.mem_unit * this->systemInfo.totalram /
+        1073741824; //GB
+    freeBytes = this->systemInfo.mem_unit * this->systemInfo.freeram /
+        1073741824; // GB
+    maxUsage = this->systemInfo.mem_unit * this->systemInfo.freeram *
+        0.5; // bytes
     this->maxVolumes = maxUsage / this->dataSize;
-    std::cout << "DEBUG: Total memory: " << totalBytes << "GB" << std::endl;
-    std::cout << "DEBUG: Free memory:  " << freeBytes << "GB" << std::endl;
-    std::cout << "DEBUG: Max to use:   " << maxUsage/1073741824 << "GB" << std::endl;
-    std::cout << "DEBUG: Concurrent vols: " << this->maxVolumes << std::endl;
 }
 
 void TimeSeries::setMaxMemory(unsigned int gigabytes)
@@ -65,24 +61,25 @@ void TimeSeries::setMaxMemory(unsigned int gigabytes)
         std::cerr << "available. Keeping limit at previous value" << std::endl;
         return;
     }
-    this->maxVolumes = 1073741824 * gigabytes / this->dataSize;
-    std::cout << "DEBUG: New maxVolumes: " << this->maxVolumes << std::endl;
+    unsigned long bytes = 1073741824 * gigabytes;
+    if(bytes < this->dataSize) {
+        std::cerr << "WARNING: Asking to use less memory than a single volume ";
+        std::cerr << "requires. Keeping limit at previous value" << std::endl;
+        return;
+    }
+    this->maxVolumes = bytes / this->dataSize;
 }
 
 void TimeSeries::encache(unsigned int index)
 {
     if(this->lruCache.size() >= this->maxVolumes) {
-        std::cout << "DEBUG: Cache is full " << this->lruCache.size();
-        std::cout << " " << this->maxVolumes << std::endl;
         // check if the LRU is this index
         if(this->lruCache.front() == index) {
-            std::cout << "DEBUG: this was the LRU " << index << std::endl;
             this->lruCache.remove(index);
             // and put it at the end
             this->lruCache.push_back(index);
         }
         else {
-            std::cout << "DEBUG: deleting LRU " << this->lruCache.front() << std::endl;
             // delete the LRU volume
             delete this->volumes[this->lruCache.front()];
             this->volumes[this->lruCache.front()] = NULL;
@@ -90,7 +87,6 @@ void TimeSeries::encache(unsigned int index)
         }
     }
     else {
-        std::cout << "DEBUG: pushed to back " << index << std::endl;
         // remove this index if it was already here
         this->lruCache.remove(index);
         // and put it at the end
