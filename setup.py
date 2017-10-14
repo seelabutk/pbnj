5012a03d2184ef19533e01868e43e2995e71676a
Tanner Hobson <thobson2@vols.utk.edu>
2017-10-14 11:20:56 -0400

    Streamline Python module installation

--- setup.py
+++ setup.py
@@ -4,7 +4,29 @@
 """
 
 from distutils.core import setup, Extension
+from os import environ
 
+
+# Needed so that the extension can always find the modules, even if they aren't
+# on your normal load path. e.g. /opt/embree/lib usually isn't in your
+# ldconfig, so this hard codes /opt/embree/lib into your extension, so that it
+# knows where to look for the libraries.
+def patch_ld_run_path():
+    if 'LD_RUN_PATH' in environ is not None:
+        ld_run_path += environ['LD_RUN_PATH'] + ':'
+    else:
+        ld_run_path = ''
+
+    ld_run_path += ':'.join([
+        '/opt/embree/lib',
+        '/usr/local/lib',
+        '/usr/local/lib64',
+    ])
+
+    environ['LD_RUN_PATH'] = ld_run_path
+
+
+patch_ld_run_path()
 
 _pbnj = Extension(
     '_pbnj',
@@ -30,17 +52,10 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
-    packages=[
-    ],
     python_requires='>=3.5',
-    install_requires=[
-    ],
     entry_points={
     },
     ext_modules=[
         _pbnj,
-    ],
-    py_modules=[
-        'python/pbnj',
     ],
 )
