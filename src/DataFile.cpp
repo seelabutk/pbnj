5fca37091ed8153e29e75e13936e30629d31794e
Alok Hota <ahota@vols.utk.edu>
2018-02-23 08:39:10 -0600

    components support for mem-mapped files

--- src/DataFile.cpp
+++ src/DataFile.cpp
@@ -87,9 +87,28 @@ void DataFile::loadFromFile(std::string filename, std::string var_name,
         }
         else {
             if(memmap) {
-                int fd = fileno(dataFile);
-                this->data = (float *)mmap(NULL, this->numValues*sizeof(float),
-                        PROT_READ, MAP_SHARED, fd, 0);
+                if(this->numComponents == 1) {
+                    int fd = fileno(dataFile);
+                    this->data = (float *)mmap(NULL, this->numValues*sizeof(float),
+                            PROT_READ, MAP_SHARED, fd, 0);
+                }
+                else {
+                    this->data = (float *)malloc(this->numValues * sizeof(float));
+                    if(this->data == MAP_FAILED) {
+                        switch(errno) {
+                            case ENOMEM:
+                                std::cerr << "ENOMEM out of memory" << std::endl;
+                                break;
+                        }
+                    }
+                    float *chunk = (float *)malloc(this->numComponents*sizeof(float));
+                    for(int i = 0; i < this->numValues; i++) {
+                        size_t numbytes = fread(chunk, sizeof(float),
+                                this->numComponents, dataFile);
+                        this->data[i] = std::sqrt(chunk[0]*chunk[0] +
+                                chunk[1]*chunk[1] + chunk[2]*chunk[2]);
+                    }
+                }
             }
             else {
                 this->data = (float *)malloc(this->numValues * sizeof(float));
