951eff970dbb24de2148f256d506af882290697b
Alok Hota <ahota@vols.utk.edu>
2018-06-27 21:14:27 -0500

    BoundingBox is a Subject too

--- include/BoundingBox.h
+++ include/BoundingBox.h
@@ -2,13 +2,14 @@
 #define PBNJ_BOUNDINGBOX_H
 
 #include <pbnj.h>
+#include <Subject.h>
 
 #include <vector>
 
 #include <ospray/ospray.h>
 
 namespace pbnj {
-    class BoundingBox {
+    class BoundingBox : public Subject {
         public:
             BoundingBox(std::vector<long unsigned int> bounds);
             ~BoundingBox();
